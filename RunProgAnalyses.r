3929d15b5dc08e45833a408137cde5419437eec0
Zhiyu <43554058+Zhiyu-9668@users.noreply.github.com>
2022-11-28 17:51:19 +0200

    Update RunProgAnalyses.r

--- RunProgAnalyses.r
+++ RunProgAnalyses.r
@@ -18,8 +18,8 @@ Input = Input[Input$AgeEndFollow >= 0, ]
 Input = Input[Input$AgeStartFollow >= 0, ]
 Input = Input[Input$AgeEndFollow >= Input$AgeStartFollow, ]
 
-Res = data.frame(matrix(ncol = 12, nrow = 0))
-colnames(Res) = c("Disease", "Target", "Predictor", "Model", "MaxSurvTime", "Sample", "Beta", "SE", "Pval", "nEvent", "nTotSample", "AnaFlag")
+Res = data.frame(matrix(ncol = 13, nrow = 0))
+colnames(Res) = c("Disease", "Target", "Predictor", "Model", "MaxSurvTime", "Sample", "AgeBin", "Beta", "SE", "Pval", "nEvent", "nTotSample", "AnaFlag")
 
 for (item in EndPt) {
   if(item %in% colnames(Input)) {
@@ -33,125 +33,152 @@ for (item in EndPt) {
     Input[[paste(item, "_Age", sep = "")]] = as.numeric(as.difftime(as.Date(Input[[paste(item, "_Date", sep = "")]])-as.Date(Input$DateOfBirth), units = "days"))/365.25
     Input[[paste(item, "_SurvTime", sep = "")]] = Input$AgeEndFollow - Input[[paste(item, "_Age", sep = "")]]
     # ColIn = append(Covar, c(item, "AgeStartFollow", "LongScore", "Death", paste(item, "_PRS", sep = ""), paste(item, "_Age", sep = ""), paste(item, "_SurvTime", sep = ""), paste(item, "_Death", sep = ""), paste(item, "_DeathUI", sep = "")))
-    ColIn = append(Covar, c(item, "AgeStartFollow", "LongScore", "Death", paste(item, "_PRS", sep = ""), paste(item, "_Age", sep = ""), paste(item, "_SurvTime", sep = ""), paste(item, "_Death", sep = "")))
-    ItemIn = Input[, ..ColIn]
-    ItemIn = ItemIn[ItemIn[[paste(item, "_Age", sep = "")]] >= 0, ]
-    ItemIn = ItemIn[complete.cases(ItemIn), ]
-    nSample = nrow(ItemIn)
-    nEvent = sum(ItemIn[[item]])
+    ColIn = append(Covar, c(item, "AgeStartFollow", "AgeEndFollow", "LongScore", "Death", paste(item, "_PRS", sep = ""), paste(item, "_Age", sep = ""), paste(item, "_SurvTime", sep = ""), paste(item, "_Death", sep = "")))
+    EndPtAll = Input[, ..ColIn]
+    EndPtAll = EndPtAll[EndPtAll[[paste(item, "_Age", sep = "")]] >= 0, ]
+    EndPtAll = EndPtAll[complete.cases(EndPtAll), ]
+    
+    for (AgeGrp in c("All", "Lower", "Upper")) {
+      if (AgeGrp == "All") {
+        AgeBin = "AllAge"
+        ItemIn = EndPtAll
+      }
+      if (AgeGrp == "Lower") {
+        AgeBin = paste("AgeOnset<", quantile(EndPtAll$AgeEndFollow, 0.5), sep = "")
+        ItemIn = EndPtAll[EndPtAll$AgeEndFollow < quantile(EndPtAll$AgeEndFollow, 0.5),]
+      }
+      if (AgeGrp == "Upper") {
+        AgeBin = paste("AgeOnset>=", quantile(EndPtAll$AgeEndFollow, 0.5), sep = "")
+        ItemIn = EndPtAll[EndPtAll$AgeEndFollow >= quantile(EndPtAll$AgeEndFollow, 0.5),]
+      }
+      nSample = nrow(ItemIn)
+      nEvent = sum(ItemIn[[item]])
 
-    Predictor = paste("scale(", item, "_PRS)", sep = "")
-    Flag = 0
-    form = paste(item," ~ ", Predictor, " + ", CovarList, sep = "")
-    m = withCallingHandlers(tryCatch({glm(as.formula(form), data = ItemIn, family = binomial, na.action=na.exclude)}, 
-                                     error = function(e){Flag <<- 2; return(NULL)}),
-                            warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
-    if (!is.null(m)) {
-      b = summary(m)$coefficient[Predictor, "Estimate"]
-      se = summary(m)$coefficient[Predictor, "Std. Error"]
-      p = summary(m)$coefficient[Predictor, "Pr(>|z|)"]
-      Res[nrow(Res) + 1,] = c(item, "Incidence", Predictor, "Logit", 999, "All", b, se, p, nEvent, nSample, Flag)
-      rm(m, b, se, p)
-    }
-    else {
-      Res[nrow(Res) + 1,] = c(item, "Incidence", Predictor, "Logit", 999, "All", "-", "-", "-", nEvent, nSample, Flag)
+      Predictor = paste("scale(", item, "_PRS)", sep = "")
+      Flag = 0
+      form = paste(item," ~ ", Predictor, " + ", CovarList, sep = "")
+      m = withCallingHandlers(tryCatch({glm(as.formula(form), data = ItemIn, family = binomial, na.action=na.exclude)}, 
+                                       error = function(e){Flag <<- 2; return(NULL)}),
+                              warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
+      if (!is.null(m)) {
+        b = summary(m)$coefficient[Predictor, "Estimate"]
+        se = summary(m)$coefficient[Predictor, "Std. Error"]
+        p = summary(m)$coefficient[Predictor, "Pr(>|z|)"]
+        Res[nrow(Res) + 1,] = c(item, "Incidence", Predictor, "Logit", 999, "All", AgeBin, b, se, p, nEvent, nSample, Flag)
+        rm(m, b, se, p)
+      }
+      else {
+        Res[nrow(Res) + 1,] = c(item, "Incidence", Predictor, "Logit", 999, "All", AgeBin, "-", "-", "-", nEvent, nSample, Flag)
+      }
+
+      Flag = 0
+      form = paste("Surv(", item, "_Age, ", item, ") ~ ", Predictor, " + ", CovarList, sep = "")
+      m = withCallingHandlers(tryCatch({coxph(as.formula(form), data = ItemIn, na.action=na.exclude)}, 
+                                       error = function(e){Flag <<- 2; return(NULL)}),
+                              warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
+      if (!is.null(m)) {
+        b = summary(m)$coefficient[Predictor,"coef"]
+        se = summary(m)$coefficient[Predictor,"se(coef)"]
+        p = summary(m)$coefficient[Predictor,"Pr(>|z|)"]
+        Res[nrow(Res) + 1,] = c(item, "TimeToOnset", Predictor, "CoxPh", 999, "All", AgeBin, b, se, p, nEvent, nSample, Flag)
+        rm(m, b, se, p)
+      }
+      else {
+        Res[nrow(Res) + 1,] = c(item, "TimeToOnset", Predictor, "CoxPh", 999, "All", AgeBin, "-", "-", "-", nEvent, nSample, Flag)
+      }
     }
 
-    Flag = 0
-    form = paste("Surv(", item, "_Age, ", item, ") ~ ", Predictor, " + ", CovarList, sep = "")
-    m = withCallingHandlers(tryCatch({coxph(as.formula(form), data = ItemIn, na.action=na.exclude)}, 
-                                     error = function(e){Flag <<- 2; return(NULL)}),
-                            warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
-    if (!is.null(m)) {
-      b = summary(m)$coefficient[Predictor,"coef"]
-      se = summary(m)$coefficient[Predictor,"se(coef)"]
-      p = summary(m)$coefficient[Predictor,"Pr(>|z|)"]
-      Res[nrow(Res) + 1,] = c(item, "TimeToOnset", Predictor, "CoxPh", 999, "All", b, se, p, nEvent, nSample, Flag)
-      rm(m, b, se, p)
-    }
-    else {
-      Res[nrow(Res) + 1,] = c(item, "TimeToOnset", Predictor, "CoxPh", 999, "All", "-", "-", "-", nEvent, nSample, Flag)
-    }
+    EndPtCase = EndPtAll[EndPtAll[[item]] == 1,]
+    for (AgeGrp in c("All", "Lower", "Upper")) {
+      if (AgeGrp == "All") {
+        AgeBin = "AllAge"
+        ItemIn = EndPtCase
+      }
+      if (AgeGrp == "Lower") {
+        AgeBin = paste("AgeOnset<", quantile(EndPtCase[[paste(item, "_Age", sep = "")]], 0.5), sep = "")
+        ItemIn = EndPtCase[EndPtCase[[paste(item, "_Age", sep = "")]] < quantile(EndPtCase[[paste(item, "_Age", sep = "")]], 0.5),]
+      }
+      if (AgeGrp == "Upper") {
+        AgeBin = paste("AgeOnset>=", quantile(EndPtCase[[paste(item, "_Age", sep = "")]], 0.5), sep = "")
+        ItemIn = EndPtCase[EndPtCase[[paste(item, "_Age", sep = "")]] >= quantile(EndPtCase[[paste(item, "_Age", sep = "")]], 0.5),]
+      }
 
-    ItemIn = ItemIn[ItemIn[[item]] == 1,]
-    for (SurvMax in c(2, 5, 10, 999)) {
-      for (Sample in c('All', 'DiagAfterJoin')) {
-        data = ItemIn[(ItemIn[[paste(item, "_SurvTime", sep = "")]] > SurvMin) & (ItemIn[[paste(item, "_SurvTime", sep = "")]] < SurvMax), ]
-        if (Sample == 'DiagAfterJoin') {
-          data = data[data$AgeStartFollow > data[[paste(item, "_Age", sep = "")]], ]
-        }
-        data = data[complete.cases(data), ]
-        nSample = nrow(data)
-        for (Predictor in c(paste("scale(", item, "_PRS)", sep = ""), "scale(LongScore)")) {
-          # for (Target in c("Death", paste(item, "_Death", sep = ""), paste(item, "_DeathUI", sep = ""))) {
-          for (Target in c("Death", paste(item, "_Death", sep = ""))) {
-            nEvent = sum(data[[Target]])
-            for (AnaCovar in c(CovarList, paste(CovarList, "+", item, "_Age", sep = ""))) {
-              model = "CoxPh"
-              if (AnaCovar == CovarList) {
-                model = "CoxPh-NoDiagAge"
-              }
-              Flag = 0
-              form = paste("Surv(", item, "_SurvTime, ", Target,") ~ ", Predictor, " + ", AnaCovar, sep = "")
-              m = withCallingHandlers(tryCatch({coxph(as.formula(form), data = data, na.action=na.exclude)}, 
-                                               error = function(e){Flag <<- 2; return(NULL)}),
-                                      warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
-              if (!is.null(m)) {
-                b = summary(m)$coefficient[Predictor,"coef"]
-                se = summary(m)$coefficient[Predictor,"se(coef)"]
-                p = summary(m)$coefficient[Predictor,"Pr(>|z|)"]
-                Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, b, se, p, nEvent, nSample, Flag)
-                rm(m, b, se, p)
-              }
-              else {
-                Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, "-", "-", "-", nEvent, nSample, Flag)
-              }
+      for (SurvMax in c(2, 5, 10, 999)) {
+        for (Sample in c('All', 'DiagAfterJoin')) {
+          data = ItemIn[(ItemIn[[paste(item, "_SurvTime", sep = "")]] > SurvMin) & (ItemIn[[paste(item, "_SurvTime", sep = "")]] < SurvMax), ]
+          if (Sample == 'DiagAfterJoin') {
+            data = data[data$AgeStartFollow > data[[paste(item, "_Age", sep = "")]], ]
+          }
+          data = data[complete.cases(data), ]
+          nSample = nrow(data)
+          for (Predictor in c(paste("scale(", item, "_PRS)", sep = ""), "scale(LongScore)")) {
+            # for (Target in c("Death", paste(item, "_Death", sep = ""), paste(item, "_DeathUI", sep = ""))) {
+            for (Target in c("Death", paste(item, "_Death", sep = ""))) {
+              nEvent = sum(data[[Target]])
+              for (AnaCovar in c(CovarList, paste(CovarList, "+", item, "_Age", sep = ""))) {
+                model = "CoxPh"
+                if (AnaCovar == CovarList) {
+                  model = "CoxPh-NoDiagAge"
+                }
+                Flag = 0
+                form = paste("Surv(", item, "_SurvTime, ", Target,") ~ ", Predictor, " + ", AnaCovar, sep = "")
+                m = withCallingHandlers(tryCatch({coxph(as.formula(form), data = data, na.action=na.exclude)}, 
+                                                 error = function(e){Flag <<- 2; return(NULL)}),
+                                        warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
+                if (!is.null(m)) {
+                  b = summary(m)$coefficient[Predictor,"coef"]
+                  se = summary(m)$coefficient[Predictor,"se(coef)"]
+                  p = summary(m)$coefficient[Predictor,"Pr(>|z|)"]
+                  Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, AgeBin, b, se, p, nEvent, nSample, Flag)
+                  rm(m, b, se, p)
+                }
+                else {
+                  Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, AgeBin, "-", "-", "-", nEvent, nSample, Flag)
+                }
 
-              model = "Logit"
-              if (AnaCovar == CovarList) {
-                model = "Logit-NoDiagAge"
-              }
-              Flag = 0
-              form = paste(Target," ~ ", Predictor, " + ", AnaCovar, sep = "")
-              m = withCallingHandlers(tryCatch({glm(as.formula(form), data = data, family = binomial, na.action=na.exclude)}, 
-                                               error = function(e){Flag <<- 2; return(NULL)}),
-                                      warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
-              if (!is.null(m)) {
-                b = summary(m)$coefficient[Predictor, "Estimate"]
-                se = summary(m)$coefficient[Predictor, "Std. Error"]
-                p = summary(m)$coefficient[Predictor, "Pr(>|z|)"]
-                Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, b, se, p, nEvent, nSample, Flag)
-                rm(m, b, se, p)
-              }
-              else {
-                Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, "-", "-", "-", nEvent, nSample, Flag)
+                model = "Logit"
+                if (AnaCovar == CovarList) {
+                  model = "Logit-NoDiagAge"
+                }
+                Flag = 0
+                form = paste(Target," ~ ", Predictor, " + ", AnaCovar, sep = "")
+                m = withCallingHandlers(tryCatch({glm(as.formula(form), data = data, family = binomial, na.action=na.exclude)}, 
+                                                 error = function(e){Flag <<- 2; return(NULL)}),
+                                        warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
+                if (!is.null(m)) {
+                  b = summary(m)$coefficient[Predictor, "Estimate"]
+                  se = summary(m)$coefficient[Predictor, "Std. Error"]
+                  p = summary(m)$coefficient[Predictor, "Pr(>|z|)"]
+                  Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, AgeBin, b, se, p, nEvent, nSample, Flag)
+                  rm(m, b, se, p)
+                }
+                else {
+                  Res[nrow(Res) + 1,] = c(item, Target, Predictor, model, SurvMax, Sample, AgeBin, "-", "-", "-", nEvent, nSample, Flag)
+                }
               }
             }
           }
-        }
-        nEvent = nSample
-        Target = paste(item, "_Age", sep = "")
-        Predictor = paste("scale(", item, "_PRS)", sep = "")
-        Flag = 0
-        form = paste(Target," ~ ", Predictor, " + ", CovarList, sep = "")
-        m = withCallingHandlers(tryCatch({lm(as.formula(form), data = data, na.action=na.exclude)}, 
-                                         error = function(e){Flag <<- 2; return(NULL)}),
-                                warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
-        if (!is.null(m)) {
-          b = summary(m)$coefficient[Predictor, "Estimate"]
-          se = summary(m)$coefficient[Predictor, "Std. Error"]
-          p = summary(m)$coefficient[Predictor, "Pr(>|t|)"]
-          Res[nrow(Res) + 1,] = c(item, Target, Predictor, "Linear", SurvMax, Sample, b, se, p, nEvent, nSample, Flag)
-          rm(m, b, se, p)
-        }
-        else {
-          Res[nrow(Res) + 1,] = c(item, Target, Predictor, "Linear", SurvMax, Sample, "-", "-", "-", nEvent, nSample, Flag)
+          nEvent = nSample
+          Target = paste(item, "_Age", sep = "")
+          Predictor = paste("scale(", item, "_PRS)", sep = "")
+          Flag = 0
+          form = paste(Target," ~ ", Predictor, " + ", CovarList, sep = "")
+          m = withCallingHandlers(tryCatch({lm(as.formula(form), data = data, na.action=na.exclude)}, 
+                                           error = function(e){Flag <<- 2; return(NULL)}),
+                                  warning=function(w) {Flag <<- 1; invokeRestart("muffleWarning")})
+          if (!is.null(m)) {
+            b = summary(m)$coefficient[Predictor, "Estimate"]
+            se = summary(m)$coefficient[Predictor, "Std. Error"]
+            p = summary(m)$coefficient[Predictor, "Pr(>|t|)"]
+            Res[nrow(Res) + 1,] = c(item, Target, Predictor, "Linear", SurvMax, Sample, AgeBin, b, se, p, nEvent, nSample, Flag)
+            rm(m, b, se, p)
+          }
+          else {
+            Res[nrow(Res) + 1,] = c(item, Target, Predictor, "Linear", SurvMax, Sample, AgeBin, "-", "-", "-", nEvent, nSample, Flag)
+          }
         }
       }
     }
   }
 }
 fwrite(Res, OutFile, quote = F, row.names = F, sep = "\t")
-  
-  
-  
